31fda1a08683e8532f5d4ec1c74f110d35a7eb77
Jayaram <Jayaram@gmail.com>
2025-05-15 16:26:51 +0530

    hello world started

---
+++ 02-coversations.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo " Trumph: Hey Musk, How are you doing"
+echo "Musk: Hello Trumph, I am good. How are you?"
+echo "Trumph: I am fine too. What's up"
+echo "Musk: Nothing much. Just going to mars now. will you join" 
+echo "Trumph: sorry, you carry on.
