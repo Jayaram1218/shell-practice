b035f383f9144b0d6204111a2813a9a9459dd6ab
Jayaram <Jayaram@gmail.com>
2025-05-17 19:17:19 +0530

    hello world started

---
+++ 08-special-vars.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "all vaiables passed to the script: $@"
+
+echo "Number of vaiable: $#"
+
+echo "Script name: $0"
+
+echo "present working directory: $PWD"
+
+echo "Home directory of the user who is runnung the script: $HOME"
+
+echo "Which user is running the script: $$"
+
+echo "PID of the last command running in background: $!"
